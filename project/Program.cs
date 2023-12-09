6031ff3c55a7ef239844a03cf37bed8bd9aa97b5
KLkapelka <153184633+KLkapelka@users.noreply.github.com>
2023-12-09 11:40:58 +0700

    Create Program.cs

---
+++ project/Program.cs
@@ -0,0 +1,22 @@
+﻿// дан файл numsTask1.txt со словами. Выведите все слова нечетной длины;
+
+using System;
+using System.IO;
+
+class work_5_1
+{
+    static void Main()
+    {
+        string[] lines = File.ReadAllLines("numsTask1.txt"); 
+        string[] words = lines[0].Split(' '); 
+
+        foreach (string i in words)
+        {
+            if (i.Length % 2 != 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+ 
